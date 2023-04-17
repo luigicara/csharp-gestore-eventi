de16638f9b381c84f8de9385dcb2fe99c07c3cf2
luigicara <luigi.caravelli98@gmail.com>
2023-04-17 11:59:23 +0200

    Milestone 1 e 2 complete

---
+++ Evento.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Evento
+    {
+        private string _titolo;
+        public string Titolo 
+        { 
+            get
+            {
+                return _titolo;
+            } 
+            set
+            {
+                if (string.IsNullOrEmpty(value)) {
+                    throw new ArgumentNullException("Il campo \"Titolo\" non può essere vuoto!");
+                } else
+                {
+                    _titolo = value;
+                }
+            }
+        }
+
+        private DateOnly _data;
+        public DateOnly Data 
+        { 
+            get 
+            { 
+                return _data;
+            } 
+            set
+            {
+                if (value < DateOnly.FromDateTime(DateTime.Now)) { 
+                    throw new ArgumentOutOfRangeException("La data di un evento non può essere antecedente ad oggi!");
+                } else
+                {
+                    _data = value;
+                }
+            }
+        }
+        private int _numPosti;
+        public int NumPosti 
+        { 
+            get
+            {
+                return _numPosti;
+            } 
+            private set
+            {
+                if (value < 0 == true) {
+                    throw new ArgumentOutOfRangeException("Il numero di posti non può essere inferiore a zero!"); 
+                } else
+                {
+                    _numPosti = value;
+                }
+            } 
+        }
+        public int PostiPrenotati { get; private set; }
+
+        public Evento(string titolo, DateOnly data, int numPosti)
+        {
+            Titolo = titolo;
+            Data = data;
+            NumPosti = numPosti;
+            PostiPrenotati = 0;
+        }
+
+        public void PrenotaPosti (int postiDaPrenotare)
+        {
+            if (Data < DateOnly.FromDateTime(DateTime.Now))
+            {
+                throw new ArgumentOutOfRangeException("L'evento si è gia svolto, impossibile prenotare nuovi posti");
+            }
+
+            if (PostiPrenotati + postiDaPrenotare > NumPosti)
+            {
+                throw new ArgumentOutOfRangeException($"L'evento non ha abbastanza posti disponibili, sono ancora disponibili {NumPosti - PostiPrenotati} posti");
+            }
+
+            PostiPrenotati += postiDaPrenotare;
+        }
+
+        public void DisdiciPosti(int postiDaRimuovere)
+        {
+            if (Data < DateOnly.FromDateTime(DateTime.Now))
+            {
+                throw new ArgumentOutOfRangeException("L'evento si è gia svolto, impossibile rimuovere posti");
+            }
+
+            if (postiDaRimuovere > PostiPrenotati)
+            {
+                throw new ArgumentOutOfRangeException($"Impossibile rimuovere più posti di quanti prenotati, sono stati prenotati {PostiPrenotati} posti");
+            }
+
+            PostiPrenotati -= postiDaRimuovere;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString() + " - " + Titolo;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,66 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Inserisci il nome dell'evento: ");
+            string titolo = Console.ReadLine();
+
+            Console.Write("Inserisci la data dell'evento: ");
+            string data = Console.ReadLine();
+
+            Console.Write("Inserisci il numero di posti totali: ");
+            int numeroPosti;
+
+            while (!int.TryParse(Console.ReadLine(), out numeroPosti))
+                Console.WriteLine("Inserisci un NUMERO!");
+
+            Evento evento = new Evento(titolo, DateOnly.ParseExact(data, "dd/MM/yyyy"), numeroPosti);
+
+            Console.Write("Quanti posti vuoi prenotare? ");
+
+            int postiDaPrenotare;
+
+            while (!int.TryParse(Console.ReadLine(), out postiDaPrenotare))
+                Console.WriteLine("Inserisci un NUMERO!");
+
+            evento.PrenotaPosti(postiDaPrenotare);
+
+            Console.WriteLine($"Numero di posti prenotati: {evento.PostiPrenotati}");
+
+            Console.WriteLine($"Numero di posti disponibili: {evento.NumPosti - evento.PostiPrenotati}");
+
+            string scelta;
+
+            do
+            {
+                Console.Write("\r\nVuoi disdire dei posti (si/no)? ");
+
+                scelta = Console.ReadLine() ?? string.Empty;
+
+                while ((scelta != "si" && scelta != "no") || string.IsNullOrEmpty(scelta))
+                {
+                    Console.WriteLine("Inserisci scelta VALIDA!");
+                    scelta = Console.ReadLine() ?? string.Empty;
+                }
+
+                if (scelta == "si")
+                {
+                    Console.Write("Inserisci il numero di posti da disdire: ");
+                    int numeroPostiDaDisdire;
+
+                    while (!int.TryParse(Console.ReadLine(), out numeroPostiDaDisdire))
+                        Console.WriteLine("Inserisci un NUMERO!");
+
+                    evento.DisdiciPosti(numeroPostiDaDisdire);
+                } else
+                    Console.WriteLine("Ok va bene!");
+
+                Console.WriteLine($"\r\nNumero di posti prenotati: {evento.PostiPrenotati}");
+
+                Console.WriteLine($"Numero di posti disponibili: {evento.NumPosti - evento.PostiPrenotati}");
+
+            } while (scelta == "si");
+
+            
         }
     }
 }
